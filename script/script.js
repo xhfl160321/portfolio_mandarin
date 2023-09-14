4956a659c7ad9f0cdae1944e9e692c3fa36ef753
sueng0422 <sueng0422@naver.com>
2023-09-14 17:26:03 +0900

    commit scroll jq

--- script/script.js
+++ script/script.js
@@ -73,4 +73,14 @@
             index = 0;
         }
     }
+
+    //클릭 시 부드러운 scroll down
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+
 })(jQuery);
